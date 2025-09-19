6238cd887655b2a892052d9b527a0650fa377c15
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-19 16:05:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Path8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\myFolder\file1.txt";
+
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+        }
+    }
+}
